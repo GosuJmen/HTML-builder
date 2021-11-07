cce9f8ebb47785535ab5312461877915780c57ef
GosuJmen <GosuJmen@gmail.com>
2021-11-07 21:08:29 +0300

    feat: 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs').promises;
 
+const filePathFrom = path.join(__dirname, 'files')
+const filePathDest = path.join(__dirname, 'files-copy')
+
+fs.readdir(filePathFrom, (err, files) => {
+  if (err)
+    console.log(err);
+  else {
+    fsPromises.mkdir(filePathDest, { recursive: true })
+    files.forEach(file => {
+      fsPromises.copyFile(`${filePathFrom}\\${file}`, `${filePathDest}\\${file}`)
+    })
+  }
+})
